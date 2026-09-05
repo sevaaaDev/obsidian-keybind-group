5e937a7f33ab0390e53393bde7cf7ca98482cde1
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2026-09-05 14:09:51 +0700

    update style of the modal

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import {
 } from 'obsidian';
 
 // TODO: style the help modal
+// TODO: handle modifier
 
 interface KeyMap {
     [key: string]: string;
@@ -36,39 +37,6 @@ export default class KeybindGroup extends Plugin {
                 new WhichKey(this, this.mainKeymap).open();
             },
         });
-        // This adds an editor command that can perform some operation on the current editor instance
-        this.addCommand({
-            id: 'replace-selected',
-            name: 'Replace selected content',
-            editorCallback: (
-                editor: Editor,
-                _ctx: MarkdownView | MarkdownFileInfo,
-            ) => {
-                editor.replaceSelection('Sample editor command');
-            },
-        });
-        // This adds a complex command that can check whether the current state of the app allows execution of the command
-        this.addCommand({
-            id: 'open-modal-complex',
-            name: 'Open modal (complex)',
-            checkCallback: (checking: boolean) => {
-                // Conditions to check
-                const markdownView =
-                    this.app.workspace.getActiveViewOfType(MarkdownView);
-                if (markdownView) {
-                    // If checking is true, we're simply "checking" if the command can be run.
-                    // If checking is false, then we want to actually perform the operation.
-                    if (!checking) {
-                        new WhichKey(this, this.mainKeymap).open();
-                    }
-
-                    // This command will only show up in Command Palette when the check function returns true
-                    return true;
-                }
-                return false;
-            },
-        });
-
     }
 
     onunload() {}
@@ -86,13 +54,31 @@ class WhichKey extends Modal {
         this.plugin = plugin;
         this.keymap = keymap;
     }
-    
+
+    render() {
+	this.setTitle('Which Key');
+	
+	const container = this.contentEl;
+	container.addClasses(['container']);
+
+	// Create grid container
+	const group = container.createDiv('group');
+
+	const header = group.createEl('h1');
+	header.setText("Editing")
+	const list = group.createEl('ul');
+	// Data rows
+	Object.entries(this.keymap).forEach(([k, c], index) => {
+	    const li = list.createEl('li');
+	    const key = li.createEl('code');	    
+	    const text = li.createEl('span');
+	    key.setText(k);
+	    text.setText(c);
+	});    	
+    }
     onOpen() {
-        this.setTitle("Which Key");
+	this.render();
         const { contentEl } = this;
-        const list = contentEl.createEl("ul");
-        Object.entries(this.keymap)
-            .forEach(([k, c]) => list.createEl("li").setText(`${k} -> ${c}`));
         
         this.handleKey = (evt: KeyboardEvent) => {
             evt.preventDefault();
